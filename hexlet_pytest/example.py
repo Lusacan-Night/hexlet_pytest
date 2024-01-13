74acf75d8189978a3bf7df5ad6869cf042f7f696
Sultan <lusacan.night@gmail.com>
2024-01-13 19:34:50 +0400

    test

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,15 @@
-def reverse(string):
-    return string[::-1]
+def fill(collection, value, begin=0, end=None):
+    length = len(collection)
+
+    normalized_end = length if end is None or end > length else end
+
+    if begin < 0:
+        return collection
+
+    for i in range(begin, normalized_end):
+        collection[i] = value
+
+coll = [1, 2, 3, 4]
+
+fill(coll, '*', 0, 10)
+print(coll)
